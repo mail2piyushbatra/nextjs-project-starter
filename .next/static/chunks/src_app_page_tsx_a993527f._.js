e6e8c65ebc30aafcb6d17dd0281997fd79a9f190
Piyush batra <59729789+mail2piyushbatra@users.noreply.github.com>
2025-08-03 15:29:49 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1754215188476

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_798d4f08._.js",
+  "static/chunks/node_modules_593bd493._.js"
+],
+    source: "dynamic"
+});
